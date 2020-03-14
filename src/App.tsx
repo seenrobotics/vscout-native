83fd7b4728e73eea9de54f99178105206c03c169
Sean D'Souza <sdammobubbles@gmail.com>
2020-03-14 15:20:22 -0400

    feat: add redux to project in js

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,28 @@
 import React from 'react';
 import {Platform, StatusBar, StyleSheet, Text, View} from 'react-native';
 
+import {Provider} from 'react-redux';
+import store from './store';
+
 import AppNavigator from './navigation/AppNavigator';
 
 const App: () => React$Node = () => {
-  return (<><View style = {styles.container}><AppNavigator /></View></>);
+  return (
+    <>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <AppNavigator />
+        </View>
+      </Provider>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-  container : {
-    flex : 1,
-    backgroundColor : '#fff',
-  }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
 });
 
 export default App;
