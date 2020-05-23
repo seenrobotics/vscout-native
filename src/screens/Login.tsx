bc35f99e1a7e7c8dd1e9d02a01fe1765679b6b82
Jeffrey Li <dr.cheddar@outlook.com>
2020-05-23 19:36:42 -0400

    login style refactor

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,21 +1,13 @@
-import * as shape from 'd3-shape';
+
 import React from 'react';
-import {Image, SafeAreaView, StyleSheet, Button, View, Dimensions, ScrollView } from 'react-native';
-// import {Button} from 'react-native-elements'
-import {Line} from 'react-native-svg';
-import {LineChart, Path} from 'react-native-svg-charts';
+import {Image, SafeAreaView, StyleSheet, ActivityIndicator, View, Dimensions, ScrollView } from 'react-native';
 
 import {Block, Text, Header} from '../components';
-import {CardList} from '../components/matches';
 import * as theme from '../constants/theme';
 import * as mocks from '../mocks';
 import {types, actions} from '../store';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
-
-import IoniconsIcon from 'react-native-vector-icons/Ionicons';
 import {NavigationStackProp} from 'react-navigation-stack';
-import { TabView, TabBar } from 'react-native-tab-view';
 
 import {connect} from 'react-redux'; 
 import { User_Credentials, Database_Credentials } from '../store/user/types';
@@ -38,95 +30,91 @@ interface StateProps {
 type Props = OwnProps & DispatchProps & StateProps;
 
 class Login extends React.Component<Props, {error_text ?: string }> {
-    public static defaultProps = {
-        user: mocks.user,
-      };
-     state = {
+  public static defaultProps = {
+      user: mocks.user,
+  };
+  state = {
          error_text : " "
-     }
-    async attemptAuthorize () {
-        try {
-            this.props.authUser();
-        }
-        catch (error)
-        {
-            this.setState({...this.state, error_text:  `LOGIN FAILED : ${error.message}`});
-        }
+  }
+  async attemptAuthorize () {
+    try {
+      this.props.authUser();
     }
-      renderHeader() {
-        const {user} = this.props;
-        // const {error_text} = this.state;
-    
-        return (
-          <Block flex={0.42} column style={{paddingHorizontal: 15}}>
-              <Block flex={false} row style={{paddingVertical: 15}}>
-              <Block center>
-                
-                <Text h3 white style={{fontSize:21, marginRight: -(100 + 10 + 30)}}>
-                  {/* {error_text || ""} */}
-                </Text>
-              </Block>
-              <View style={{width:100, marginRight:10}}>
-                <Text h4 white style={{fontSize:10, textAlign:'right'}}></Text>
-                <Text h3 white style={{fontSize:15, textAlign:'right'}}></Text>
-              </View>
-              <Image style={styles.avatar} source={user.avatar} />
-            </Block>
-         
-           <TouchableOpacity
-           style={styles.button}
-            onPress={() => this.attemptAuthorize()}
-            >
-               <Text h3 white style={{fontSize:18, marginTop : 10, marginBottom : 10}}>
-                  LOGIN TO VSCOUT
-                  <Image
-      source={ { uri: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/91_Discord_logo_logos-512.png' }}
-      style={[
-        {
-          transform: [{ scaleX:  false ? -1 : 1 }],
-        },
-        {
-          width: 30,
-          height: 30,
-        //   tintColor: "white"
-        }
-      ]}
-    />
-                </Text>
-                
-                </TouchableOpacity>
+    catch (error)
+    {
+      this.setState({...this.state, error_text:  `LOGIN FAILED : ${error.message}`});
+    }
+  }
+
+  renderHeader() {
+    const {user} = this.props;
+    const {error_text} = this.state;
+
+    return (
+      <Block flex={0.42} column style={{paddingHorizontal: 15}}>
+        <Block flex={false} row style={{paddingVertical: 15}}>
+          <Block center>
+            <Text h3 white style={{fontSize:21, marginRight: -(100 + 10 + 30)}}>
+              {error_text || ""}
+            </Text>
           </Block>
-        );
-      }
-    render() {
-        return (
-          <SafeAreaView style={styles.safe}>
-                {this.renderHeader()}
-          </SafeAreaView>
-        );
-      }
+          <View style={{width:100, marginRight:10}}>
+            <Text h4 white style={{fontSize:10, textAlign:'right'}}></Text>
+            <Text h3 white style={{fontSize:15, textAlign:'right'}}></Text>
+          </View>
+          <Image style={styles.avatar} source={user.avatar} />
+        </Block>
+      
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.attemptAuthorize()}
+        >
+          <Text h3 white style={{fontSize:18, marginTop : 10, marginBottom : 10}}>
+            LOGIN TO VSCOUT
+            <Image 
+              source={ { uri: 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/91_Discord_logo_logos-512.png' }}
+              style={styles.logo}
+            />
+          </Text>
+        </TouchableOpacity>
+      </Block>
+    );
+  }
+  render() 
+  {
+    return (
+      <SafeAreaView style={styles.safe}>
+            {this.renderHeader()}
+      </SafeAreaView>
+    );
+  }
 }
 const mapStateToProps = (state: types.RootState) => ({
-    user_credentials : state.user.user_credentials,
-    database_credentials : state.user.database_credentials,
-    user : state.user.user,
-  });
+  user_credentials : state.user.user_credentials,
+  database_credentials : state.user.database_credentials,
+  user : state.user.user,
+});
   
 export default connect(mapStateToProps, { authUser })(Login);
 
 const styles = StyleSheet.create({
-    safe: {flex: 1, backgroundColor: theme.colors.primary,  },
-    button : {
-        alignItems: "center",
-        padding: 5,
-        backgroundColor : "#7289DA"
-    },
-    headerChart: {paddingTop: 30, paddingBottom: 30, zIndex: 1},
+  safe: {flex: 1, backgroundColor: theme.colors.primary,  },
+  button : {
+      alignItems: "center",
+      padding: 5,
+      backgroundColor : "#7289DA"
+  },
+  logo : {
+    transform: [{ scaleX:  false ? -1 : 1 }],
+    width: 30,
+    height: 30,
+  },
+  headerChart: {paddingTop: 30, paddingBottom: 30, zIndex: 1},
   avatar: {
     width: 30,
     height: 30,
     borderRadius: 30 / 2,
     marginRight: 5,
   },
-  });
+});
   
