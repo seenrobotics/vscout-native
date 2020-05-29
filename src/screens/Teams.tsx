1756fb6e0eca6e84fa6658fd3d754cdf1da224b6
Eshwar Chock <eshchock1@gmail.com>
2020-05-29 10:50:34 -0400

    incorporated search bar on teams page

--- src/screens/Teams.tsx
+++ src/screens/Teams.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, SafeAreaView, StyleSheet, View} from 'react-native';
+import {Image, SafeAreaView, StyleSheet, View,} from 'react-native';
 import {Block, Text} from '../components';
 import {CardList} from '../components/teams';
 import * as theme from '../constants/theme';
@@ -8,6 +8,7 @@ import {NavigationTabProp} from 'react-navigation-tabs';
 import {connect} from 'react-redux';
 import {types, actions} from '../store';
 import { TouchableHighlight } from 'react-native-gesture-handler';
+import { SearchBar } from 'react-native-elements';
 
 interface OwnProps {
     type: string;
@@ -24,13 +25,22 @@ export class Teams extends React.Component<Props, {}> {
         userTeam: mocks.userTeam,
         teams: mocks.teams,
       };
+      state = {
+        search: '',
+      };
+
+      updateSearch = search => {
+        this.setState({ search });
+      };
+
     
       renderHeader() {
         const {user} = this.props;
         const {userTeam} = this.props;
         const {teams} = this.props;
+        const {search} = this.state;
         return (
-          <Block flex={0.42} column style={{paddingHorizontal: 15}}>
+          <Block flex={0.7} column style={{paddingHorizontal: 15}}>
               <Block flex={false} row style={{paddingVertical: 15}}>
               <Block center>
                 <Text h3 white style={{fontSize:21, marginRight: -(100 + 10 + 30)}}>
@@ -43,8 +53,20 @@ export class Teams extends React.Component<Props, {}> {
               </View>
               <Image style={styles.avatar} source={user.avatar} />
             </Block>
-            <Block>
-            <Block row card shadow color={theme.colors.accent} style={styles.headerChart}>
+            <Block flex={false} style={{paddingBottom:20,}}>
+            <SearchBar
+            platform = "default"
+            lightTheme = {true}
+            round = {true}
+            containerStyle = {{backgroundColor:"transparent", borderBottomColor:"transparent", borderTopColor:"transparent", padding:0, margin:0,}}
+            inputContainerStyle = {{backgroundColor:theme.colors.white,}}
+            inputStyle={{color:theme.colors.darkgray, fontFamily: "Montserrat-Medium",}}
+            placeholder="Search Teams"
+            onChangeText={this.updateSearch}
+            value={search}
+            />
+            </Block>
+            <Block flex={false} row card shadow color={theme.colors.accent} style={styles.headerChart}>
                 <Block flex={1}>
         <Block flex={3} color="" style={{justifyContent: 'center',}}><Text h1 style={{paddingLeft:15, fontSize:38, color:"white",}}>{userTeam.teamOrg + userTeam.teamLetter}</Text></Block>
         <Block flex={2} color="" style={{justifyContent: 'flex-start',}}><Text h3 style={{paddingLeft:15, fontSize:15,color:"white",}}>{userTeam.location}</Text></Block>
@@ -63,7 +85,6 @@ export class Teams extends React.Component<Props, {}> {
                   <Text h3 style={{color:"white", fontSize:13, paddingBottom:11, paddingTop:5,}}>Skills Score</Text>
                   </Block>
                 </Block>
-            </Block>
             </Block>
           </Block>
         );
