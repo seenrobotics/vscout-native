861f1b66779f19df9b8d03ac9806b0a5416406d6
Eshwar Chock <eshchock1@gmail.com>
2020-05-29 10:50:34 -0400

    incorporated search bar on teams page

--- src/components/teams/CardList.tsx
+++ src/components/teams/CardList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, TouchableOpacity, ScrollView} from 'react-native';
+import {StyleSheet, TouchableOpacity, ScrollView,} from 'react-native';
 import {Text, Block} from '../index';
 import {EventData} from '../../store/events/types';
 import Utils from '../../utils';
