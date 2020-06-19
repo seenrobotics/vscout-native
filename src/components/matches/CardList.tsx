d4573b4448bcffcfef1c2d2f75d6b57b481e3222
Eshwar Chock <eshchock1@gmail.com>
2020-06-19 18:57:19 -0400

    Revert "Pr fixes"
    This reverts commit 095d56053f4f8c28936c91b04acdf491f9fbb646.

--- src/components/matches/CardList.tsx
+++ src/components/matches/CardList.tsx
@@ -14,6 +14,7 @@ export interface CardListProps {
   navigation : NavigationStackProp<NavigationRoute<NavigationParams>>,
 }
 const CardList : (props : CardListProps) => React.ReactElement = (props: CardListProps) => {
+  console.log("asdasdasdadsad" + props)
   const {matches, type} = props;
   const cardRenders : Array<MatchDoc> = matches;
   return (
